b4f348e4f2daeca38f2734889a25e58bd72e1ff4
Pravalika <pravalikanaroju1@gmail.com>
2023-11-07 12:29:05 +0530

    My Activities Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { HarvestiveComponent } from './harvestive/harvestive.component';
 import { AboutUsiComponent } from './about-usi/about-usi.component';
 import { ExpertsComponent } from './experts/experts.component';
-<<<<<<< HEAD
 import { MyActivitiesComponent } from './myactivities/myactivities.component';
-=======
 import { MyAppointmentsComponent } from './my-appointments/my-appointments.component';
->>>>>>> 1cb103d836a2906f0c678b42580e950ba66fe3b5
 
 const routes: Routes = [
   {path:'sales',component:SalesOverviewComponent},
   {path:'harvestive',component:HarvestiveComponent},
   {path:'new',component:AboutUsiComponent},
   {path:'Expert',component:ExpertsComponent},
-<<<<<<< HEAD
+  {path:'aboutusi',component:AboutUsiComponent},
   {path:'MyActivities',component:MyActivitiesComponent},
 
-=======
   {
     path:'myAppointments',component:MyAppointmentsComponent
-  },
->>>>>>> 1cb103d836a2906f0c678b42580e950ba66fe3b5
+  }
 
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,9 @@ import { AboutUsiComponent } from './about-usi/about-usi.component';
 import { ChartsComponent } from './charts/charts.component';
 import { ExpertsComponent } from './experts/experts.component';
 import {MatTabsModule} from '@angular/material/tabs';
-<<<<<<< HEAD
 import { MyActivitiesComponent } from './myactivities/myactivities.component';
-=======
 
 import { MyAppointmentsComponent } from './my-appointments/my-appointments.component';
->>>>>>> 1cb103d836a2906f0c678b42580e950ba66fe3b5
 // import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService, DragAndDropService, ResizeService, WorkHoursModel} from '@syncfusion/ej2-angular-schedule';
@@ -37,12 +34,9 @@ import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWe
     AboutUsiComponent,
     ChartsComponent,
     ExpertsComponent,
-<<<<<<< HEAD
-    MyActivitiesComponent
-=======
-    MyAppointmentsComponent,
+    MyActivitiesComponent,
+    MyAppointmentsComponent
    
->>>>>>> 1cb103d836a2906f0c678b42580e950ba66fe3b5
   ],
   imports: [
     BrowserModule,
